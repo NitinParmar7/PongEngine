d7795d172a9ad5aa98cb5cf66020caa70a4c712f
Kuro <nitinprmr75@gmail.com>
2024-01-07 14:03:02 +0530

    Update Scene flow and added intro scene

---
+++ PongGame/src/scenes/IntroScene.cpp
@@ -0,0 +1,23 @@
+﻿#include "IntroScene.h"
+
+IntroScene::IntroScene()
+{
+    m_scene_id_ = 1;
+}
+
+IntroScene::~IntroScene()
+{
+    m_game_objects.clear();
+}
+
+void IntroScene::EnterScene()
+{
+}
+
+void IntroScene::UpdateScene()
+{
+}
+
+void IntroScene::ExitScene()
+{
+}
